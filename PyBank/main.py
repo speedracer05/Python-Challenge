7dcea098c724a55d03cb9ff19bdde628fd2eeb51
speedracer05 <speedracer-05@outlook.com>
2021-04-08 01:22:13 -0700

    #3 commit. Created list for rev_change, which was then used to find rev_gain, rev_decrease, started some terminal print routines.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
-#The total number of months included in the dataset
-#The net total amount of "Profit/Losses" over the entire period
-#Calculate the changes in "Profit/Losses" over the entire period, then find 
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# Calculate the changes in "Profit/Losses" over the entire period, then find 
 #  the average of those changes
 #The greatest increase in profits (date and amount) over the entire period
 #The greatest decrease in losses (date and amount) over the entire period
@@ -17,8 +17,8 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 # List of variables
 date = []         # is list
 rev = []          # is list
-rev_change = [0]  # is list
-avgrev_change = 0 # variable, set to zero
+rev_change = [0]  # is list # change
+sum_change = 0 # variable, set to zero # change sum
 rev_gain = 0      # variable, set to zero
 rev_decrease = 0  # variable, set to zero
 total_rev = 0     # variable, set to zero
@@ -41,8 +41,36 @@ with open(csvpath, newline='') as csvfile: # might need to add ''in place of...,
         rev.append(row[1])
 
 # Calculate the change in revenue over the entire period, and write to the list[rev-change]
-for r in range(0, len(rev)-1):
+for r in range(0, len(rev)-1): # <-1> subtract one row for the header
 
+    # Note: manual validation for 1st calc (984655-867884 = 116771) checks 
+    # out with print results.
     rev_change.append(int(rev[r+1]) - int(rev[r]))
-    #print(rev_change)
-    # manual validation for 1st calc checks out with terminal print: 984655-867884 = 116771
+    print(rev_change)
+
+# forLoop in the list rev_change, from zero to the end of items in rev_change
+for c in range(0, len(rev_change)):
+    
+    # find the greatest gain
+    if rev_change[c] > rev_gain:
+        rev_gain = rev_change[c]
+
+    # find the greatest decrease
+    if rev_change[c] < rev_decrease:
+        rev_decrease = rev_change[c]
+
+    # total_rev is assigned an integer value of rev in list r
+    total_rev = int(rev[r])
+
+    # Calculate the change in revenue (profit/loss). 
+    # Use abs function for absolute difference.
+    sum_change = abs(rev_change[c])
+
+# Calculate the avg change by taking the abolute value of change, divided by
+#  the number of items in rev_change, less one for the header.
+AvgChange = int(sum_change/(len(rev_change)-1))
+
+# Analysis print via Terminal
+print("Total Months: " + str(len(date)))
+print("Total Revenue: " + str(total_rev))
+print("Average Revenue Change " + str(AvgChange))
