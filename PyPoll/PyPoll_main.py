ad38b2d42a39cb438c4e2e53d66cc76d565ce6ac
speedracer05 <speedracer-05@outlook.com>
2021-05-11 23:54:06 -0700

    created .py file, deleted ipynb file.

---
+++ PyPoll/PyPoll_main.py
@@ -0,0 +1,39 @@
+import os
+import csv
+
+# Set path for file
+csvpath = os.path.join("..", "Resources", "election_data.csv"
+
+# List of variables
+
+
+
+
+
+
+# PyPoll Analysis print via Terminal
+#print("Election Results")
+#print("**************************")
+#print("")
+#print("**************************")
+#print("")
+#print("")
+#print("")
+#print("")
+#print("**************************")
+#print("")
+#print("**************************")
+
+# Write text file with results of financial analysis
+#with open("Report_PyPoll.txt", "w") as text_file:
+#    text_file.write("Election Results" + "\n")
+#    text_file.write("**************************" + "\n")
+#    text_file.write("Total Votes:" + 
+#    text_file.write("**************************" + "\n")
+#    text_file.write(
+#    text_file.write(
+#    text_file.write(
+#    text_file.write(
+#    text_file.write("**************************" + "\n")
+#    text_file.write(
+#    text_file.write("**************************" + "\n")
