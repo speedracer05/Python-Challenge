000a2b2ec5a351fe70ab022b33622426dc6631cd
speedracer05 <speedracer-05@outlook.com>
2021-05-15 09:13:11 -0700

    vote count by candidate

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -15,36 +15,35 @@ csvpath = os.path.join("Resources", "election_data.csv")
 # List of Variables
 total_votes = 0     # Set int value to zero
 candidate_list = [] # Set empty list for candidate
-candidate_count = []
+candidate_count = {}
 count_vlist = []    # Set empty list for voter count
 election_file = ["election_data.csv"]
 highest_votes = 0   # Set int value to zero for winning votes
 percent = []
 
-# Set iteration through rows in election file
+# Iterate through rows in election file
 for file in election_file:
     
-# Open file
+    # Open file
     with open(csvpath, 'r') as file:
         csvreader = csv.reader(file, delimiter=',')
         csv_header = next(csvreader)
-# Iterate rows in csv, increment total votes list by 1 for each row
+        # Iterate rows, increment total votes list by 1 & get candidate name
         for row in csvreader:
             total_votes = total_votes+1
-            candidate_name = row[2]
-# Populate candidate list with unique candidate names, then increment voter count list by 1
-            if not candidate_name in candidate_list:
+            candidate_name = row[2] 
+            # Add unique candidate names, and increment voter count list by 1
+            if candidate_name not in candidate_list:
                 candidate_list.append(candidate_name)
-                count_vlist.append(1)
-            
-            # else:
-            #     vote_sum = candidate_list.index(candidate_name)
-            #     pointer_candidate = count_vlist[vote_sum]
-            #     count_vlist[vote_sum] = vote_sum
+                candidate_count[candidate_name] = 0
+            candidate_count[candidate_name] += 1
+
+
 
 print(total_votes)
 print(candidate_list)
-# print(count_vlist)
+print(candidate_count)
+
 # print(str(total_votes))
 
 # for candidate_name in candidate_list:
