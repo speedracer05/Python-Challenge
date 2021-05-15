20057fe16ccf0fe41c4c77f5023f2d45ae323b60
speedracer05 <speedracer-05@outlook.com>
2021-05-15 09:34:15 -0700

    create outputpath for txt, terminal print in-prog

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -1,8 +1,8 @@
 # pypoll
 # ! The total number of votes cast
-# A complete list of candidates who received votes
+# ! A complete list of candidates who received votes
 # The percentage of votes each candidate won
-# The total number of votes each candidate won
+# ! The total number of votes each candidate won
 # The winner of the election based on popular vote.
 # Final script should both print the analysis to the terminal and export a text file
 
@@ -10,14 +10,13 @@ import os
 import csv
 
 # Set path for file
-csvpath = os.path.join("Resources", "election_data.csv")
+csvpath = os.path.join('Resources', 'election_data.csv')
 
 # List of Variables
-total_votes = 0     # Set int value to zero
-candidate_list = [] # Set empty list for candidate
+total_votes = 0
+candidate_list = []
 candidate_count = {}
-count_vlist = []    # Set empty list for voter count
-election_file = ["election_data.csv"]
+election_file = ['election_data.csv']
 highest_votes = 0   # Set int value to zero for winning votes
 percent = []
 
@@ -40,17 +39,18 @@ for file in election_file:
 
 
 
-print(total_votes)
+print()
 print(candidate_list)
 print(candidate_count)
 
-# print(str(total_votes))
 
-# for candidate_name in candidate_list:
-#     candidate_count.append((total_votes.count(candidate_name))
-#     percent.append(round(total_votes.count(candidate_name)/total_votes*100,3))
+outputpath = os.path.join('Analysis','Results.txt')
 
-# for candidate_name in candidate_list:
-#     votes = count_vlist[candidate_list.index(candidate_name)]
-#     # print(votes)
-#     vote_percent = (votes/total_votes)*100
+resultsfile = open(outputpath, 'w')
+
+# Election Results
+print('')
+print('PyPoll Election Analysis')
+print('-'*26)
+print(f'Total Votes: {total_votes}')
+print('-'*26)
