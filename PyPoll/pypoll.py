1f824531e7791f82d40234f4a91c4b22979b1b32
speedracer05 <speedracer-05@outlook.com>
2021-05-15 22:48:01 -0700

    removed [row] from candidate forloop

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -8,6 +8,7 @@
 
 import os
 import csv
+from types import GetSetDescriptorType
 
 # Set path for file
 csvpath = os.path.join('Resources', 'election_data.csv')
@@ -18,7 +19,7 @@ candidate_list = []
 candidate_count = {}
 election_file = ['election_data.csv']
 highest_votes = 0   # Set int value to zero for winning votes
-percent = []
+# percent = []
 
 # Iterate through rows in election file
 for file in election_file:
@@ -36,11 +37,13 @@ for file in election_file:
             candidate_count[candidate_name] += 1
         for candidate_name in candidate_count:
             votes = candidate_count[candidate_name]
-            percent = int(votes)/int(total_votes)*100                
+            percent = int(votes)/int(total_votes)*100
+            candidate_results = (
+                f'{candidate_name}: {percent:.1f}% ({votes:,})\n'
+            )
+        # winner = candidate_list[candidate_count.index(max(candidate_count))]
 
 print(candidate_results)
-
-
 
 outputpath = os.path.join('Analysis','Results.txt')
 
@@ -51,10 +54,10 @@ print('PyPoll Election Analysis')
 print('-'*26)
 print(f'Total Votes: {total_votes}')
 print('-'*26)
-for row in range(len(candidate_list)):
-    print(f'{candidate_name[row]}: {percent[row]}% {votes[row]})\n'
-            )
+# for row in range(len(candidate_list)):
+#     print(f'{candidate_name[row]}: {percent[row]}% {votes[row]})\n'
+#             )
 # for row in range(len(candidate_list)):
 #     print(f'{candidate_list[row]}: {percent[row]}% {candidate_count[row]}')
-# print(candidate_count)
+print(candidate_count)
 print('-'*26)
